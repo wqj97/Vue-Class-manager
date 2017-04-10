dd4c9daf059f0725ce658a8fac6566df1d86d6ad
wqj97 <wqj97@126.com>
2017-04-10 08:58:42 +0800

    back-end init

---
+++ front-end/src/components/Before.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Before">
+    <file-upload title="课前预习" :content="content"></file-upload>
+  </div>
+</template>
+
+<script>
+  import FileUpload from './FileUpload.vue'
+  export default {
+    name: 'Before',
+    data () {
+      return {
+        content: ''
+      }
+    },
+    components: {
+      FileUpload
+    }
+  }
+</script>
+
+<style lang="scss" type="text/scss">
+  .Before{
+  }
+</style>
